4aefe96382ec37fdf38c4e3158f4e83a2c793f03
Nestor <148357514+nPreissler@users.noreply.github.com>
2025-01-07 21:27:23 -0300

    uploading project to remote repo

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+// sort a enemy in a list 
+// sort attacks in a list
+// /--> 1 medium, 1 stronger, 1 weak
+
+// start a fight
+// 1 round for player, 1 for enemy /--> sort for a weak, strong or medium attack
+// if player losts game ask to restart, else the game asks to close or sort enemy again
+
+let enemiesObject = [
+    {
+        name: "Dragon",
+        attacks: {
+            dragon_rage: 40,
+            bite: 15,
+            roar: 5
+        }
+    },
+    {
+        name: "Troll",
+        attacks: {
+            smash: 35,
+            kick: 25,
+            punch: 5
+        }
+    },
+    {
+        name: "Zombie",
+        attacks: {
+            bite: 30,
+            poison: 20,
+            punch: 5
+        }
+    }
+]
+
+function sortEnemie() {
+    return enemiesObject[Math.floor(Math.random() * enemiesObject.length)]
+}
+
+const sortedEnemie = sortEnemie()
+
+function sortAttack() {
+    return sortedEnemie.attacks[Math.floor(Math.random() * sortedEnemie.attacks.length)]
+}
+
